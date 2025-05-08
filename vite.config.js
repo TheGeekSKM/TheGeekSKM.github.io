f179a83fe452e9cf38214a9534607e54889329c4
Sai Mangipudi <thegeekskm101@gmail.com>
2025-05-08 00:28:58 -0500

    Created Code Snippet Template

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
+import prismjs from 'vite-plugin-prismjs';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  plugins: [
+    prismjs({
+      languages: ['csharp', 'javascript'],
+      plugins: ['line-numbers', 'copy-to-clipboard','line-numbers', 'line-highlight'],
+      theme: 'github-dark',
+      css: true,
+    })
+  ],
   build: {
     rollupOptions: {
       input: {
         main: 'index.html',
         novels: 'novels.html',
-        novel: 'novel.html'
+        novel: 'novel.html',
+        codeSnippet: 'code-snippet.html',
       },
       external: ['novels/**/*.md'], // Force Vite to keep markdown files
     },
